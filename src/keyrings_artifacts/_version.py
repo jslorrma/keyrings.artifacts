0792dfce54abc7682d3f13218666f5784edfd775
jslorrma <jslorrma@gmail.com>
2024-08-25 14:43:44 +0200

    Release v0.4.0

--- src/keyrings_artifacts/_version.py
+++ src/keyrings_artifacts/_version.py
@@ -19,6 +19,6 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = "0.3.2"  # version string
+__version__ = "0.4.0"  # version string
 vesion = __version__
 __version_tuple__ = version_tuple = tuple(int(n) for n in __version__.split(".") if n.isdigit())
